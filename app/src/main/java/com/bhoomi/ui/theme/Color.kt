d2b95de74a25f2eef952f11a84e4a1fd3e6bddfd
vishal <vishalvidhate196@gmail.com>
2023-05-20 02:55:56 +0530

    update design

--- app/src/main/java/com/bhoomi/ui/theme/Color.kt
+++ app/src/main/java/com/bhoomi/ui/theme/Color.kt
@@ -9,6 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
 val BG = Color(0XFF154992)
 val BGLite = Color(0xFFE9EEF2)
 val CardBG = Color(0x8AE5F3FF)
